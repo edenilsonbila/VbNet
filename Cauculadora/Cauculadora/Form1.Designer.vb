f2da44c6108a5e0f8131a5ffbe5c48192a92225b
edenilsonbila <edenilsonbila@yahoo.com>
2021-03-18 00:35:48 -0300

    Adicionado Projeto Calculador
    Famosa calculadora, com o diferencial da tela de resultados em 2, tentando simular a calculadora nativa do windows

---
+++ Cauculadora/Cauculadora/Form1.Designer.vb
@@ -0,0 +1,278 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.tbvlr2 = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.tbvlr1 = New System.Windows.Forms.TextBox()
+        Me.btnvezes = New System.Windows.Forms.Button()
+        Me.btnigual = New System.Windows.Forms.Button()
+        Me.btndivisao = New System.Windows.Forms.Button()
+        Me.btnmais = New System.Windows.Forms.Button()
+        Me.btnmenos = New System.Windows.Forms.Button()
+        Me.btnponto = New System.Windows.Forms.Button()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.btn6 = New System.Windows.Forms.Button()
+        Me.btn9 = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.btn5 = New System.Windows.Forms.Button()
+        Me.btn8 = New System.Windows.Forms.Button()
+        Me.btn0 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.btn4 = New System.Windows.Forms.Button()
+        Me.btn7 = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'tbvlr2
+        '
+        Me.tbvlr2.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.tbvlr2.Location = New System.Drawing.Point(12, 38)
+        Me.tbvlr2.Name = "tbvlr2"
+        Me.tbvlr2.Size = New System.Drawing.Size(194, 13)
+        Me.tbvlr2.TabIndex = 0
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.tbvlr1)
+        Me.GroupBox1.Controls.Add(Me.btnvezes)
+        Me.GroupBox1.Controls.Add(Me.btnigual)
+        Me.GroupBox1.Controls.Add(Me.btndivisao)
+        Me.GroupBox1.Controls.Add(Me.btnmais)
+        Me.GroupBox1.Controls.Add(Me.btnmenos)
+        Me.GroupBox1.Controls.Add(Me.btnponto)
+        Me.GroupBox1.Controls.Add(Me.btn3)
+        Me.GroupBox1.Controls.Add(Me.btn6)
+        Me.GroupBox1.Controls.Add(Me.btn9)
+        Me.GroupBox1.Controls.Add(Me.btn2)
+        Me.GroupBox1.Controls.Add(Me.btn5)
+        Me.GroupBox1.Controls.Add(Me.btn8)
+        Me.GroupBox1.Controls.Add(Me.btn0)
+        Me.GroupBox1.Controls.Add(Me.Button3)
+        Me.GroupBox1.Controls.Add(Me.btn4)
+        Me.GroupBox1.Controls.Add(Me.btn7)
+        Me.GroupBox1.Controls.Add(Me.tbvlr2)
+        Me.GroupBox1.Controls.Add(Me.Panel1)
+        Me.GroupBox1.Location = New System.Drawing.Point(13, 13)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(220, 309)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        '
+        'tbvlr1
+        '
+        Me.tbvlr1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.tbvlr1.Location = New System.Drawing.Point(12, 19)
+        Me.tbvlr1.Name = "tbvlr1"
+        Me.tbvlr1.Size = New System.Drawing.Size(194, 13)
+        Me.tbvlr1.TabIndex = 18
+        '
+        'btnvezes
+        '
+        Me.btnvezes.Location = New System.Drawing.Point(162, 265)
+        Me.btnvezes.Name = "btnvezes"
+        Me.btnvezes.Size = New System.Drawing.Size(44, 38)
+        Me.btnvezes.TabIndex = 17
+        Me.btnvezes.Text = "*"
+        Me.btnvezes.UseVisualStyleBackColor = True
+        '
+        'btnigual
+        '
+        Me.btnigual.Location = New System.Drawing.Point(162, 78)
+        Me.btnigual.Name = "btnigual"
+        Me.btnigual.Size = New System.Drawing.Size(44, 170)
+        Me.btnigual.TabIndex = 16
+        Me.btnigual.Text = "="
+        Me.btnigual.UseVisualStyleBackColor = True
+        '
+        'btndivisao
+        '
+        Me.btndivisao.Location = New System.Drawing.Point(12, 265)
+        Me.btndivisao.Name = "btndivisao"
+        Me.btndivisao.Size = New System.Drawing.Size(44, 38)
+        Me.btndivisao.TabIndex = 15
+        Me.btndivisao.Text = "/"
+        Me.btndivisao.UseVisualStyleBackColor = True
+        '
+        'btnmais
+        '
+        Me.btnmais.Location = New System.Drawing.Point(62, 265)
+        Me.btnmais.Name = "btnmais"
+        Me.btnmais.Size = New System.Drawing.Size(44, 38)
+        Me.btnmais.TabIndex = 14
+        Me.btnmais.Text = "+"
+        Me.btnmais.UseVisualStyleBackColor = True
+        '
+        'btnmenos
+        '
+        Me.btnmenos.Location = New System.Drawing.Point(112, 265)
+        Me.btnmenos.Name = "btnmenos"
+        Me.btnmenos.Size = New System.Drawing.Size(44, 38)
+        Me.btnmenos.TabIndex = 13
+        Me.btnmenos.Text = "-"
+        Me.btnmenos.UseVisualStyleBackColor = True
+        '
+        'btnponto
+        '
+        Me.btnponto.Location = New System.Drawing.Point(112, 210)
+        Me.btnponto.Name = "btnponto"
+        Me.btnponto.Size = New System.Drawing.Size(44, 38)
+        Me.btnponto.TabIndex = 12
+        Me.btnponto.Text = "C"
+        Me.btnponto.UseVisualStyleBackColor = True
+        '
+        'btn3
+        '
+        Me.btn3.Location = New System.Drawing.Point(112, 166)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(44, 38)
+        Me.btn3.TabIndex = 11
+        Me.btn3.Text = "3"
+        Me.btn3.UseVisualStyleBackColor = True
+        '
+        'btn6
+        '
+        Me.btn6.Location = New System.Drawing.Point(112, 122)
+        Me.btn6.Name = "btn6"
+        Me.btn6.Size = New System.Drawing.Size(44, 38)
+        Me.btn6.TabIndex = 10
+        Me.btn6.Text = "6"
+        Me.btn6.UseVisualStyleBackColor = True
+        '
+        'btn9
+        '
+        Me.btn9.Location = New System.Drawing.Point(112, 78)
+        Me.btn9.Name = "btn9"
+        Me.btn9.Size = New System.Drawing.Size(44, 38)
+        Me.btn9.TabIndex = 9
+        Me.btn9.Text = "9"
+        Me.btn9.UseVisualStyleBackColor = True
+        '
+        'btn2
+        '
+        Me.btn2.Location = New System.Drawing.Point(62, 166)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(44, 38)
+        Me.btn2.TabIndex = 8
+        Me.btn2.Text = "2"
+        Me.btn2.UseVisualStyleBackColor = True
+        '
+        'btn5
+        '
+        Me.btn5.Location = New System.Drawing.Point(62, 122)
+        Me.btn5.Name = "btn5"
+        Me.btn5.Size = New System.Drawing.Size(44, 38)
+        Me.btn5.TabIndex = 7
+        Me.btn5.Text = "5"
+        Me.btn5.UseVisualStyleBackColor = True
+        '
+        'btn8
+        '
+        Me.btn8.Location = New System.Drawing.Point(62, 78)
+        Me.btn8.Name = "btn8"
+        Me.btn8.Size = New System.Drawing.Size(44, 38)
+        Me.btn8.TabIndex = 6
+        Me.btn8.Text = "8"
+        Me.btn8.UseVisualStyleBackColor = True
+        '
+        'btn0
+        '
+        Me.btn0.Location = New System.Drawing.Point(12, 210)
+        Me.btn0.Name = "btn0"
+        Me.btn0.Size = New System.Drawing.Size(94, 38)
+        Me.btn0.TabIndex = 5
+        Me.btn0.Text = "0"
+        Me.btn0.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(12, 166)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(44, 38)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "1"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'btn4
+        '
+        Me.btn4.Location = New System.Drawing.Point(12, 122)
+        Me.btn4.Name = "btn4"
+        Me.btn4.Size = New System.Drawing.Size(44, 38)
+        Me.btn4.TabIndex = 3
+        Me.btn4.Text = "4"
+        Me.btn4.UseVisualStyleBackColor = True
+        '
+        'btn7
+        '
+        Me.btn7.Location = New System.Drawing.Point(12, 78)
+        Me.btn7.Name = "btn7"
+        Me.btn7.Size = New System.Drawing.Size(44, 38)
+        Me.btn7.TabIndex = 2
+        Me.btn7.Text = "7"
+        Me.btn7.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.White
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel1.Location = New System.Drawing.Point(6, 11)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(208, 48)
+        Me.Panel1.TabIndex = 19
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(245, 334)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Cauculadora"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents tbvlr2 As System.Windows.Forms.TextBox
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents btnigual As System.Windows.Forms.Button
+    Friend WithEvents btndivisao As System.Windows.Forms.Button
+    Friend WithEvents btnmais As System.Windows.Forms.Button
+    Friend WithEvents btnmenos As System.Windows.Forms.Button
+    Friend WithEvents btnponto As System.Windows.Forms.Button
+    Friend WithEvents btn3 As System.Windows.Forms.Button
+    Friend WithEvents btn6 As System.Windows.Forms.Button
+    Friend WithEvents btn9 As System.Windows.Forms.Button
+    Friend WithEvents btn2 As System.Windows.Forms.Button
+    Friend WithEvents btn5 As System.Windows.Forms.Button
+    Friend WithEvents btn8 As System.Windows.Forms.Button
+    Friend WithEvents btn0 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents btn4 As System.Windows.Forms.Button
+    Friend WithEvents btn7 As System.Windows.Forms.Button
+    Friend WithEvents btnvezes As System.Windows.Forms.Button
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents tbvlr1 As System.Windows.Forms.TextBox
+
+End Class
---
+++ Cauculadora/Cauculadora/Form1.vb
@@ -0,0 +1,144 @@
+﻿Public Class Form1
+
+    'Variaveis
+
+    Public vlr1 As Double
+    Public vlr2 As Double
+    Public resultado As String
+    Public operação As String
+
+
+    Private Sub tbvlhr1_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles tbvlr1.KeyPress, tbvlr1.KeyPress
+        Dim allowedChars As String = ""
+        If allowedChars.IndexOf(e.KeyChar) = -1 Then
+            e.Handled = True
+        End If
+    End Sub
+
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.CenterToScreen()
+        tbvlr2.Select()
+        tbvlr1.TextAlign = HorizontalAlignment.Right
+        tbvlr2.TextAlign = HorizontalAlignment.Right
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        tbvlr2.Text += Convert.ToString(1)
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn4.Click
+        tbvlr2.Text += Convert.ToString(4)
+    End Sub
+
+    Private Sub btn7_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn7.Click
+        tbvlr2.Text += Convert.ToString(7)
+    End Sub
+
+    Private Sub btn8_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn8.Click
+        tbvlr2.Text += Convert.ToString(8)
+    End Sub
+
+    Private Sub btn5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn5.Click
+        tbvlr2.Text += Convert.ToString(5)
+    End Sub
+
+    Private Sub btn2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn2.Click
+        tbvlr2.Text += Convert.ToString(2)
+    End Sub
+
+    Private Sub btn0_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn0.Click
+        tbvlr2.Text += Convert.ToString(0)
+    End Sub
+
+    Private Sub btn9_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn9.Click
+        tbvlr2.Text += Convert.ToString(9)
+    End Sub
+
+    Private Sub btn6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn6.Click
+        tbvlr2.Text += Convert.ToString(6)
+    End Sub
+
+    Private Sub btn3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn3.Click
+        tbvlr2.Text += Convert.ToString(3)
+    End Sub
+
+    Private Sub btnigual_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnigual.Click
+        If tbvlr1.Text = "" Then
+        ElseIf tbvlr2.Text = "" Then
+        Else
+
+
+
+            vlr2 = tbvlr2.Text
+
+            Select Case operação
+                Case "/"
+                    resultado = vlr1 / vlr2
+                Case "+"
+                    resultado = vlr1 + vlr2
+                Case "-"
+                    resultado = vlr1 - vlr2
+                Case "*"
+                    resultado = vlr1 * vlr2
+            End Select
+
+            'tbvlr1.Text = ""
+            tbvlr2.Text = ""
+            tbvlr2.Text = resultado
+        End If
+    End Sub
+
+    Private Sub btndivisao_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btndivisao.Click
+        If tbvlr2.Text = "" Then
+            MsgBox("Campo em branco")
+        Else
+            vlr1 = tbvlr2.Text
+            operação = "/"
+            tbvlr1.Text = vlr1 & operação
+            tbvlr2.Text = ""
+            tbvlr2.Select()
+        End If
+    End Sub
+
+    Private Sub btnmais_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnmais.Click
+        If tbvlr2.Text = "" Then
+            MsgBox("Campo em branco")
+        Else
+            vlr1 = tbvlr2.Text
+            operação = "+"
+            tbvlr1.Text += vlr1 & operação
+            tbvlr2.Text = ""
+            tbvlr2.Select()
+        End If
+    End Sub
+
+    Private Sub btnmenos_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnmenos.Click
+        If tbvlr2.Text = "" Then
+            MsgBox("Campo em branco")
+        Else
+            vlr1 = tbvlr2.Text
+            operação = "-"
+            tbvlr1.Text = vlr1 & operação
+            tbvlr2.Text = ""
+            tbvlr2.Select()
+        End If
+    End Sub
+
+    Private Sub btnvezes_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnvezes.Click
+        If tbvlr2.Text = "" Then
+            MsgBox("Campo em branco")
+        Else
+            vlr1 = tbvlr2.Text
+            operação = "*"
+            tbvlr1.Text = vlr1 & operação
+            tbvlr2.Text = ""
+            tbvlr2.Select()
+        End If
+    End Sub
+
+    Private Sub btnponto_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnponto.Click
+        tbvlr1.Text = ""
+        tbvlr2.Text = ""
+    End Sub
+End Class
