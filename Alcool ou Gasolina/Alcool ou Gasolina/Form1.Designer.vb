ff85a706a0be159dd1b38b0897e90792b42deeff
edenilsonbila <edenilsonbila@yahoo.com>
2021-03-18 00:32:12 -0300

    Commit Inicial

---
+++ Alcool ou Gasolina/Alcool ou Gasolina/Form1.Designer.vb
@@ -0,0 +1,121 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblResultado = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.tbAlcool = New System.Windows.Forms.MaskedTextBox()
+        Me.tbGasolina = New System.Windows.Forms.MaskedTextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(24, 99)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Calcular"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(2, 137)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(58, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Resultado:"
+        '
+        'lblResultado
+        '
+        Me.lblResultado.AutoSize = True
+        Me.lblResultado.Location = New System.Drawing.Point(2, 174)
+        Me.lblResultado.Name = "lblResultado"
+        Me.lblResultado.Size = New System.Drawing.Size(58, 13)
+        Me.lblResultado.TabIndex = 4
+        Me.lblResultado.Text = "Resultado:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 13)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(67, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Preço Alcool"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 57)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(79, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Preço Gasolina"
+        '
+        'tbAlcool
+        '
+        Me.tbAlcool.Location = New System.Drawing.Point(15, 34)
+        Me.tbAlcool.Mask = "0,00"
+        Me.tbAlcool.Name = "tbAlcool"
+        Me.tbAlcool.Size = New System.Drawing.Size(129, 20)
+        Me.tbAlcool.TabIndex = 7
+        '
+        'tbGasolina
+        '
+        Me.tbGasolina.Location = New System.Drawing.Point(15, 73)
+        Me.tbGasolina.Mask = "0,00"
+        Me.tbGasolina.Name = "tbGasolina"
+        Me.tbGasolina.Size = New System.Drawing.Size(129, 20)
+        Me.tbGasolina.TabIndex = 8
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(156, 216)
+        Me.Controls.Add(Me.tbGasolina)
+        Me.Controls.Add(Me.tbAlcool)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.lblResultado)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents lblResultado As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents tbAlcool As System.Windows.Forms.MaskedTextBox
+    Friend WithEvents tbGasolina As System.Windows.Forms.MaskedTextBox
+
+End Class
---
+++ Alcool ou Gasolina/Alcool ou Gasolina/Form1.vb
@@ -0,0 +1,33 @@
+﻿Imports System.Globalization
+
+Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        lblResultado.Visible = False
+        Me.CenterToScreen()
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        'Declara variaveis
+        Dim vlralcool As Decimal
+        Dim vlrgas As Decimal
+        Dim resultado As Decimal
+
+        'Atribui valor as variaveis
+        vlralcool = Convert.ToDecimal(tbAlcool.Text,CultureInfo.InvariantCulture)
+        vlrgas = Convert.ToDecimal(tbGasolina.Text, CultureInfo.InvariantCulture)
+        resultado = vlralcool / vlrgas
+
+        'Faz a operação
+        If resultado > 0.07 Then
+            lblResultado.Text = UCase("Abasteça com Alcool")
+            lblResultado.Visible = True
+            lblResultado.ForeColor = Color.Green
+        Else
+            lblResultado.Text = UCase("Abasteça com Gasolina")
+            lblResultado.Visible = True
+            lblResultado.ForeColor = Color.Green
+        End If
+    End Sub
+End Class
